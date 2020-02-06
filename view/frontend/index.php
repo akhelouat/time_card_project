34fc0d79ef557adeffdac99f325006751aaf4d22
stephane <stephane.vieira75@outlook.fr>
2020-02-06 23:11:38 +0100

    create: basic login form in index.php

--- view/frontend/index.php
+++ view/frontend/index.php
@@ -1,18 +1,33 @@
 <?php
-session_start();
-session_destroy();
+// Title
+$titlePage = "Login";
+
+//Requires
+require('header.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>OHA Présence</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></head>
-<body>
 
+<!-- Login form container -->
+<form class="container" action="frontendUser/checkPresence.php" method="POST">
+    <!-- User input -->
+    <div class="form-group">
+        <label for="usr">Username</label>
+        <input type="text" class="form-control" id="usr" name="user" required>
+    </div>
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
-</html>
+    <!-- Password input -->
+    <div class="form-group">
+        <label for="passw">Password</label>
+        <input type="password" class="form-control" id="passw" name="password" required>
+    </div>
+
+    <!-- Check input -->
+    <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="check">
+        <label class="form-check-label" for="check">Remember me</label>
+    </div>
+
+    <!-- Submit button -->
+    <button type="submit" class="btn btn-primary">login</button>
+</form>
+
+<?php require('footer.php')?>
