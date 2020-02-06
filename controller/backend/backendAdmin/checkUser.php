151dbfda09cf9da4071d2f990deab8a07aebfc0e
rahil-f <felixrahil@yahoo.fr>
2020-02-06 15:08:26 +0100

    userPseudo function

--- controller/backend/backendAdmin/checkUser.php
+++ controller/backend/backendAdmin/checkUser.php
@@ -1,6 +1,6 @@
 <?php
-
+require("../../../model/dbconnect.php");
 function userPseudo($nom, $prenom){
-    $first = substr($prenom, 0, 1);
-    return ("{$first}{$nom}");
+    $firstLetter = substr($prenom, 0, 1);
+    return ("{$firstLetter}{$nom}");
 }
