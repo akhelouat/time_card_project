cd99a70c4e4a6356d86b7c7b0e61bd2e7fb1bb2e
stephane <stephane.vieira75@outlook.fr>
2020-02-06 23:18:49 +0100

    create: now page require header.php, menu.php & footer.php in createPromo.php

--- view/frontend/frontendAdmin/createPromo.php
+++ view/frontend/frontendAdmin/createPromo.php
@@ -1,29 +1,27 @@
 <?php
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
+// Title
+$titlePage = "New User";
 
-    <title>Promo</title>
-</head>
-<body>
-    <div>
-        <form action="POST" action="checkPromo.php">
-            <label for="promo"> promo:</label>
-            <input type="text" name="promo">
-            <button>send</button>
-        </form>
-        <h2>promo list:</h2>
-        <?php
-            $res = $db->query("SELECT nom, prenom, user FROM member INNER JOIN member_info ON member.id = member_id ORDER BY id
-            ASC");
-            $res->data_seek(0);
-            while ($row = $res->fetch_assoc()) {
-             ?> <a href='userPage.php?user="<?=$row['user'];?>"'>
-             <?php echo " id = " . $row['id'] . "\n";?></a> <?php
-            }
-        ?>
-    </div>
-</body>
-</html>
+//Requires
+
+require('../menu.php');
+require('../header.php');
+?>
+<div>
+    <form action="POST" action="checkPromo.php">
+        <label for="promo"> promo:</label>
+        <input type="text" name="promo">
+        <button>send</button>
+    </form>
+    <h2>promo list:</h2>
+    <?php
+        $res = $db->query("SELECT nom, prenom, user FROM member INNER JOIN member_info ON member.id = member_id ORDER BY id
+        ASC");
+        $res->data_seek(0);
+        while ($row = $res->fetch_assoc()) {
+            ?> <a href='userPage.php?user="<?=$row['user'];?>"'>
+            <?php echo " id = " . $row['id'] . "\n";?></a> <?php
+        }
+    ?>
+</div>
+<?php require('../footer.php')?>
