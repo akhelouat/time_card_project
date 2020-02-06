7c86fb0273ff3c25efea5d50f03401ea8e2ae21c
stephane <stephane.vieira75@outlook.fr>
2020-02-06 23:16:03 +0100

    create: basic menu in menu.php

---
+++ view/frontend/menu.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <!-- Logo -->
+    <a class="navbar-brand" href="#">Time Card</a>
+    
+    <!-- Nav links -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+
+            <!-- Create promotion -->
+            <li class="nav-item">
+                <a class="nav-link" href="createPromo.php">Create promotion</a>
+            </li>
+            
+            <!-- New User -->
+            <li class="nav-item">
+                <a class="nav-link" href="newUser.php">New User</a>
+            </li>
+
+
+            <!-- Manage User -->
+            <li class="nav-item">
+                <a class="nav-link" href="manageUser.php">Manage User</a>
+            </li>
+
+
+            <!-- Manage User -->
+            <li class="nav-item">
+                <a class="nav-link" href="userPage.php">User page</a>
+            </li>
+        </ul>
+
+        <!-- Logout -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Logout</a>
+            </li>
+        </ul>
+    </div>
+</nav>
