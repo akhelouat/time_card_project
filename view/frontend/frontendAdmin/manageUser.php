53bdb7b81e28ff50aa6832936df1a0a0f28a50df
stephane <stephane.vieira75@outlook.fr>
2020-02-06 23:20:08 +0100

    create: basic template in manageUser.php, newUser.php & userPage.php

---
+++ view/frontend/frontendAdmin/manageUser.php
@@ -0,0 +1,15 @@
+<?php
+// Title
+$titlePage = "Manage User";
+
+//Requires
+
+require('../menu.php');
+require('../header.php');
+?>
+
+<div class="container">
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum necessitatibus amet vitae veritatis ea assumenda voluptate placeat nostrum quod asperiores. Accusantium voluptate beatae dignissimos culpa ex blanditiis exercitationem mollitia neque ipsum voluptatum fugiat delectus ipsam alias, natus nihil ipsa iure laudantium. Dicta libero sint eveniet deleniti fugiat perferendis tempore dolorem qui pariatur reiciendis. Ut voluptatum sed, quasi perspiciatis eius dolor facilis expedita cumque nesciunt eveniet mollitia porro suscipit alias sint iure. At iusto saepe laudantium? Vero earum suscipit provident accusamus dicta ducimus? Labore distinctio eius placeat recusandae ratione rem, ea rerum ut cupiditate numquam et cumque tempora sequi ad esse magnam nemo, expedita temporibus. Officiis, error? Est, sequi maiores recusandae inventore totam quo mollitia assumenda ratione quos vero architecto saepe laborum. A neque voluptatibus corrupti in iure nisi autem. Perspiciatis beatae perferendis deleniti necessitatibus velit repudiandae assumenda delectus earum repellendus ut libero fugit, vitae quidem voluptate, harum quaerat soluta labore eligendi! Temporibus provident maiores delectus!</p>
+</div>
+
+<?php require('../footer.php')?>
